bc3d127288db439dc2164532df39a629d5b02fd4
Su, Xiaobin <xsu@psi.de>
2021-07-21 09:23:51 +0800

    Init the project

---
+++ src/main/java/org/spring/exercise/SpringExerciseApplication.java
@@ -0,0 +1,15 @@
+package org.spring.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xsu
+ */
+@SpringBootApplication
+public class SpringExerciseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringExerciseApplication.class, args);
+  }
+}
---
+++ src/test/java/org/spring/exercise/SpringExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
